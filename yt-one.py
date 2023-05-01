30146b8fcae7b1817a1c9d55d098da62fddca0d9
rebontadeb <rebontadeb@gmail.com>
2023-05-01 09:16:55 +0530

    added shebang

--- yt-one.py
+++ yt-one.py
@@ -1,18 +1,19 @@
-!#/Users/rebontadeb/opt/anaconda3/bin/python3
+#!/Users/rebontadeb/opt/anaconda3/bin/python3
 
 from pytube import YouTube
-SAVE_PATH = "/Users/rebontadeb/Documents/My_Docs.nosync/youtube_video/" 
+
+SAVE_PATH = "/Users/rebontadeb/Documents/My_Docs.nosync/youtube_video/"
 inp = input("Prompt:")
 link = []
-#link=["https://www.youtube.com/watch?v=T4Df5_cojAs&ab_channel=kubucation"]
+# link=["https://www.youtube.com/watch?v=T4Df5_cojAs&ab_channel=kubucation"]
 link.append(inp.strip())
 
 for i in link:
-	video_obj = YouTube(i,use_oauth=True, allow_oauth_cache=True)
-	stream = video_obj.streams.get_highest_resolution() 
-	#stream = video_obj.streams.filter(only_audio=True).first()  
-	try:	
-		stream.download(SAVE_PATH)
-		print("Task Completed")        
-	except:
-		print("Some Error!")
+    video_obj = YouTube(i, use_oauth=True, allow_oauth_cache=True)
+    stream = video_obj.streams.get_highest_resolution()
+    # stream = video_obj.streams.filter(only_audio=True).first()
+    try:
+        stream.download(SAVE_PATH)
+        print("Task Completed")
+    except:
+        print("Some Error!")
