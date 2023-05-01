ea73abedc804dc044f035aed4d4abf2ae459d23b
rebontadeb <rebontadeb@gmail.com>
2023-05-01 09:17:50 +0530

    added shebang

--- yt-one.py
+++ yt-one.py
@@ -5,7 +5,6 @@ from pytube import YouTube
 SAVE_PATH = "/Users/rebontadeb/Documents/My_Docs.nosync/youtube_video/"
 inp = input("Prompt:")
 link = []
-# link=["https://www.youtube.com/watch?v=T4Df5_cojAs&ab_channel=kubucation"]
 link.append(inp.strip())
 
 for i in link:
